022e006bb3541fcda873e5166f5baf86048634ba
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-17 23:51:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,11 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :price
+      t.integer :size
+      t.string :image_url
+    end
+  end
 end
