82f1945813b77ff37bb4c73b1e99c6d91b7549bd
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-17 23:57:22 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,13 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
-  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end 
 end
