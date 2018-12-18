147bfae5de1754c5ef1120c5679ee0fa3afec4f1
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-18 00:03:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.integer :size
       t.string :image_url
       
