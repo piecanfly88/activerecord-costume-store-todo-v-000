f8841d6157568845a90ec62bc04c9ee4fd9587e3
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-18 00:00:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.integer "price"
     t.boolean "family_friend"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.string "long_description"
+    t.string "opening_date"
+    t.string "closing_date"
+    t.string "description"
   end
 
 end
