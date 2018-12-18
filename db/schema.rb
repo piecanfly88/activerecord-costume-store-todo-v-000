cb36cbe00259d4ab19825a4a82c2bd76f347bce0
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-18 00:04:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.integer "price"
-    t.string "family_friendly"
+    t.integer "family_friendly"
     t.string "opening_date"
     t.string "closing_date"
     t.string "description"
