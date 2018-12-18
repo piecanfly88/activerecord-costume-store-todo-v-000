c316dc059befecf3c018e69f3985fbbfaef0fb9b
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-12-18 00:03:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.float "price"
     t.integer "size"
     t.string "image_url"
     t.datetime "created_at", null: false
